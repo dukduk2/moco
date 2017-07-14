1671a5de67942e774409342cb88798895776fbe7
user1-PC\user1 <wognsl34@hanmail.net>
2017-07-14 15:00:52 +0900

    스트리밍 젠장

---
+++ src/main/java/com/moco/interceptor/StreamingInterCeptor.java
@@ -0,0 +1,75 @@
+package com.moco.interceptor;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.moco.member.MemberDTO;
+import com.moco.movieSchedule.MovieScheduleDTO;
+import com.moco.movieSchedule.MovieScheduleService;
+
+@Component
+public class StreamingInterCeptor extends HandlerInterceptorAdapter{
+	
+	@Autowired
+	private MovieScheduleService movieScheduleService;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("==================== 스트리밍 시간체크 ====================");
+		boolean check = false;
+
+		// Client 접속 시간
+		String id = ((MemberDTO)request.getSession().getAttribute("memberDTO")).getId();
+		Date curDate = new Date();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HHmm");
+		String clientTime = simpleDateFormat.format(curDate);
+		System.out.println(id+" 님의 접속 시간 : "+ clientTime);
+		
+		// playTime
+		MovieScheduleDTO movieScheduleDTO = movieScheduleService.sysdateMovie();
+		int playTime = movieScheduleService.getPlayTime(movieScheduleDTO.getPnum());
+
+		// 영화 startTime
+		int startH = 20;
+		int startM = 0;
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.HOUR_OF_DAY, startH);
+		calendar.set(Calendar.MINUTE, startM);
+		String movieStart =  simpleDateFormat.format(calendar.getTime());
+		System.out.println("영화 >> "+movieScheduleDTO.getTitle()+" 의 시작 시간 : "+movieStart);
+		
+		// 영화 endTime
+		calendar.add(Calendar.MINUTE, playTime);
+		String movieEnd = simpleDateFormat.format(calendar.getTime());
+		System.out.println("영화 >> "+movieScheduleDTO.getTitle()+" 의 종료 시간 : "+movieEnd);
+		
+		// 클라이언트 접속 시간이 스타트 시간~ 엔드 시간 안에 없다면 fasle.
+		int user = Integer.parseInt(clientTime);
+		int start = Integer.parseInt(movieStart);
+		int end = Integer.parseInt(movieEnd);
+		if(user>=start && user<=end){
+			check = !check;
+		}else{
+			System.out.println("스트리밍 시간 아니야");
+			response.sendRedirect("/finalProject/#go");
+		}
+		
+		return check;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		System.out.println("==================== 스트리밍 시간체크 끝 ====================");
+		super.postHandle(request, response, handler, modelAndView);
+	}
+	
+}
--- src/main/webapp/WEB-INF/views/movie/movieSchedule/movieStreaming.jsp
+++ src/main/webapp/WEB-INF/views/movie/movieSchedule/movieStreaming.jsp
@@ -10,27 +10,8 @@
 <link rel="styleSheet" type="text/css" href="${pageContext.request.contextPath}/resources/css/section.css">
 <link rel="styleSheet" type="text/css" href="${pageContext.request.contextPath}/resources/css/list.css">
 <title>Insert title here</title>
-<script type="text/javascript">
-	$(function() {
-		
-	});
-</script>
-<style type="text/css">
-	.video{
-		width: 100%;
-		height: 600px;
-	}
-</style>
 </head>
 <body>
-	<div class="container">
-		<h2 style="text-align: center;">${title}</h2>
-			<div>
-				<video autoplay="autoplay"  class="video" preload="metadata">
-					<source src="${ROOT}/resources/upload/adminMovieUpload/${fname}" type="video/mp4">
-				</video>
-			</div>	
-	</div>
 	
 </body>
 </html>
